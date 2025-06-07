36d4a2b7eb0b2cb1509fe4264fdf2b5b177323ef
Cristian Lorenzo <cristian.lorenzoremon@gmail.com>
2025-06-07 03:00:41 +0100

    Añadido el default connection

--- Cinematrix.API/Datos/CinematrixContext.cs
+++ Cinematrix.API/Datos/CinematrixContext.cs
@@ -35,9 +35,12 @@ public partial class CinematrixContext : IdentityDbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("name=DefaultConnection");
+    }
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-       // => optionsBuilder.UseSqlServer("Server=DESKTOP-49V8H3B;Database=CinematrixPRUEBA;Integrated Security=True;TrustServerCertificate=True");
+       
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
